322e7991aa526760d354d8e760267a6dea2deeba
Monika Kubianka <mkubianka@gmail.com>
2024-10-17 17:23:34 +0200

    email password validation

--- src/models/club.js
+++ src/models/club.js
@@ -1,26 +1,35 @@
 const mongoose = require("mongoose");
 const clubSchema = new mongoose.Schema({
- name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-      lowercase: true,
-      },
-    
-    password: {
-      type: String,
-      required: true,
-      minlength: 7,
-      trim: true,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
       }
+    },
+  },
 
-})
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+});
 
 const Club = mongoose.model("Club", clubSchema);
 
