1c9a9633f1f727ca892f5e117b2832c0f26d3e56
Monika Kubianka <mkubianka@gmail.com>
2024-11-04 00:50:36 +0100

    redirection after club logout

--- public/js/clublogout.js
+++ public/js/clublogout.js
@@ -2,6 +2,7 @@ const messageOne = document.querySelector("#message-1");
 
 const token = localStorage.getItem("authToken");
 messageOne.textContent = "Loading...";
+
 fetch("/clubs/logout", {
   method: "POST",
   headers: {
@@ -11,24 +12,55 @@ fetch("/clubs/logout", {
 })
   .then((response) => {
     if (response.ok) {
-      // Jeśli odpowiedź jest poprawna, ale nie ma ciała, nie parsuj jako JSON
-      if (
-        response.status === 204 ||
-        response.headers.get("Content-Length") === "0"
-      ) {
-        return null; // Nie ma treści do parsowania
-      }
-      return response.json(); // Parsuj tylko, jeśli odpowiedź ma ciało
+      // Usuwamy token po pomyślnym wylogowaniu
+      localStorage.removeItem("authToken");
+      messageOne.textContent = "User logged out";
+
+      // Przekierowanie do strony logowania
+      window.location.href = "/clublogin";
     } else {
-      throw new Error("Response not OK");
+      throw new Error("Wylogowanie nie powiodło się");
     }
   })
-  .then((data) => {
-    localStorage.removeItem("authToken");
-    console.log("User logged out");
-    messageOne.textContent = "User logged out";
-  })
-  .catch((error) => console.error("Error:", error));
+  .catch((error) => {
+    console.error("Error:", error);
+    messageOne.textContent = "An error occurred. Please try again.";
+  });
+
+//od tad
+// const messageOne = document.querySelector("#message-1");
+
+// const token = localStorage.getItem("authToken");
+// messageOne.textContent = "Loading...";
+// fetch("/clubs/logout", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer ${token}`, // Dodanie tokena do nagłówka
+//   },
+// })
+//   .then((response) => {
+//     if (response.ok) {
+//       // Jeśli odpowiedź jest poprawna, ale nie ma ciała, nie parsuj jako JSON
+//       if (
+//         response.status === 204 ||
+//         response.headers.get("Content-Length") === "0"
+//       ) {
+//         return null; // Nie ma treści do parsowania
+//       }
+//       return response.json(); // Parsuj tylko, jeśli odpowiedź ma ciało
+//     } else {
+//       throw new Error("Response not OK");
+//     }
+//   })
+//   .then((data) => {
+//     localStorage.removeItem("authToken");
+//     console.log("User logged out");
+//     messageOne.textContent = "User logged out";
+//   })
+//   .catch((error) => console.error("Error:", error));
+
+//do tad
 
 // const token = localStorage.getItem("authToken");
 
