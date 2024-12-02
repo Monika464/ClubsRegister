7cc30e6f20b3c8ddf38f9f26776e8dd47116616e
Monika Kubianka <mkubianka@gmail.com>
2024-12-02 17:47:16 +0100

    tailwind set

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,hbs}", "./public/**/*.{html,js,hbs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
